326766139311b57a3e4c34bcce412bc8652c36fb
Ivan Yakovenko <ivan.yakovenko@icloud.com>
2020-05-06 23:04:09 +0300

    update xilinx proj

--- labs/готовый/process/core.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-
-:: This is a batch file to run CoreGEN application
-
-:: Open CoreGEN with specified project
-call C:\Xilinx\bin\nt\coregen.bat -p C:\USERS\IVAN\DESKTOP\PROCES~1\LABS\PROCESS -q C:\USERS\IVAN\DESKTOP\PROCES~1\LABS\PROCESS -i C:\USERS\IVAN\DESKTOP\PROCES~1\LABS\PROCESS\core.ini
